47311865cfac2e12718931ab88a80984dc15989c
hur <3088468311@qq.com>
2025-11-19 09:07:18 +0800

    初始化项目，推送至 menu-back 分支

---
+++ src/main/java/org/example/menuback/MenuBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.menuback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MenuBackApplication.class, args);
+    }
+
+}
